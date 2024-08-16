cbcdb1be08271eb2b9c7129caa97bb77bfaacdeb
Bahar <abdullohbahar@gmail.com>
2024-08-16 20:56:51 +0700

    membuat ubah jadwal sidang

--- resources/views/admin/pengajuan/jadwal-sidang/show.blade.php
+++ resources/views/admin/pengajuan/jadwal-sidang/show.blade.php
@@ -148,6 +148,21 @@
                                                     : {{ $pengajuan->hasOneJadwalSidang->url }}
                                                 </td>
                                             </tr>
+                                            @php
+                                                $tanggalSidang = $pengajuan->hasOneJadwalSidang->getRawOriginal(
+                                                    'tanggal',
+                                                );
+                                                $tanggalSekarang = date('Y-m-d');
+                                            @endphp
+
+                                            @if ($tanggalSidang > $tanggalSekarang)
+                                                <tr>
+                                                    <td colspan="2">
+                                                        <button data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                                            class="btn btn-warning btn-sm">Ubah Jadwal Sidang</button>
+                                                    </td>
+                                                </tr>
+                                            @endif
                                         </table>
                                     </div>
                                     <div class="col-12 mt-5">
@@ -178,6 +193,97 @@
         <!--end::Content-->
     </div>
     <!--end::Content wrapper-->
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Ubah Jadwal Sidang</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="{{ route('admin.store.jadwal.sidang', $pengajuan->id) }}" id="jadwalSidang" method="POST">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-12 col-lg-6 mt-4">
+                                <label for="" class="form-label">Tanggal</label>
+                                <input type="date" class="form-control @error('tanggal') is-invalid @enderror"
+                                    value="{{ $pengajuan->hasOneJadwalSidang->tanggal }}" name="tanggal" id=""
+                                    required>
+                                @error('tanggal')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="col-sm-12 col-md-12 col-lg-6 mt-4">
+                                <label for="" class="form-label">Jam</label>
+                                <div class="input-group mb-5" style="width: 100%">
+                                    @php
+                                        $parts = explode(':', $pengajuan->hasOneJadwalSidang->jam);
+                                    @endphp
+                                    <select name="jam" class="form-control" id="">
+                                        <option value="00">00</option>
+                                        @for ($i = 1; $i < 24; $i++)
+                                            @php
+                                                $formattedValue = str_pad($i, 2, '0', STR_PAD_LEFT);
+                                            @endphp
+                                            <option {{ $parts[0] == $formattedValue ? 'selected' : '' }}
+                                                value="{{ $formattedValue }}">{{ $formattedValue }}</option>
+                                        @endfor
+                                    </select>
+                                    <select name="menit" class="form-control" id="">
+                                        <option value="00">00</option>
+                                        @for ($i = 1; $i < 60; $i++)
+                                            @php
+                                                $formattedValue = str_pad($i, 2, '0', STR_PAD_LEFT);
+                                            @endphp
+                                            <option {{ $parts[1] == $formattedValue ? 'selected' : '' }}
+                                                value="{{ $formattedValue }}">{{ $formattedValue }}</option>
+                                        @endfor
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-12 col-lg-12 mt-4">
+                                <label for="" class="form-label">Tipe Sidang</label>
+                                <select name="tipe" class="form-control @error('tipe') is-invalid @enderror"
+                                    id="tipe" required>
+                                    <option value="">-- Pilih Tipe Sidang --</option>
+                                    <option
+                                        {{ old('tipe', $pengajuan->hasOneJadwalSidang->tipe) == 'offline' ? 'selected' : '' }}
+                                        value="offline">
+                                        Offline</option>
+                                    <option
+                                        {{ old('tipe', $pengajuan->hasOneJadwalSidang->tipe) == 'online' ? 'selected' : '' }}
+                                        value="online">
+                                        Online</option>
+                                </select>
+                                @error('tipe')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="col-sm-12 col-md-12 col-lg-12 mt-4" id="alamatComponent" hidden>
+                                <label for="" class="form-label">Alamat Pelaksanaan Sidang</label>
+                                <textarea name="alamat" id="alamat" class="form-control" id="" style="width: 100%" rows="2">{{ old('alamat', $pengajuan->hasOneJadwalSidang->alamat) }}</textarea>
+                            </div>
+                            <div class="col-sm-12 col-md-12 col-lg-12 mt-4" id="urlComponent" hidden>
+                                <label for="" class="form-label">URL Sidang Online</label>
+                                <textarea name="url" id="url" class="form-control" id="" style="width: 100%" rows="2">{{ old('url', $pengajuan->hasOneJadwalSidang->url) }}</textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('addons-js')
@@ -207,5 +313,27 @@
                 });
             });
         });
+    </script>
+
+    <script>
+        $("#tipe").on("change", function() {
+            var val = $(this).val()
+
+            console.log(val)
+
+            if (val == 'offline') {
+                $("#alamatComponent").attr("hidden", false)
+                $("#alamat").attr("required", true)
+
+                $("#urlComponent").attr("hidden", true)
+                $("#url").attr("required", false)
+            } else {
+                $("#urlComponent").attr("hidden", false)
+                $("#url").attr("required", true)
+
+                $("#alamatComponent").attr("hidden", true)
+                $("#alamat").attr("required", false)
+            }
+        }).trigger("change")
     </script>
 @endpush
